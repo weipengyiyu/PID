230849848bc18027ea8dfdb866d16ce2692c0a55
weipeng <3214969473@qq.com>
2020-09-17 17:41:58 +0800

    第二版

--- PID/main.c
+++ PID/main.c
@@ -12,73 +12,61 @@ PID 算法是一种工控领域常见的控制算法，用于闭环反馈控制。有以下两种分类：
 我们使用高级语言的思想去实现两种 PID ，做到对于用户来说，调用相同的接口，
 内部实现不同的 PID 算法
 */
-#define VAL_MAX (100)
 
 enum PID_MODE
 {
-	PID_INC  = 0,	//增量式
-	PID_POS,		//位置式
+	PID_INC,
+	PID_POS
 };
 
-struct PID
+typedef struct PID
 {
 	enum PID_MODE mode;
 
-	float	kp;
-	float	ki;
-	float	kd;
+	float		kp;
+	float		ki;
+	float		kd;
 
-	double	targetPoint;
-	double	currError;
-	double	lastError;
-	double	prevError;
+	double		targetPoint;
+	double		currentval;
+	double		lastval;
+	double		last_lastval;
+	double		iSum;
 
-	void 	(*init)(struct PID *this, double targetPoint);
-	double	(*outputLimit)(struct PID *this, double output);
-	void 	(*setParameter)(struct PID *this, float kp, float ki, float kd);
-	double	(*calculate)(struct PID *this, double currenPoint);
-};
+	void		(*init)(struct PID *this, float targetPoint, int mode);
+	void		(*setParameter)(struct PID *this, float kp, float ki, float kd);
+	double		(*claculate)(struct PID *this, float currentPoint);
+}pid_ctrl;
 
-struct PID_INC
+static void PID_init(pid_ctrl *this, float targetPoint, int mode)
 {
-	struct PID pid;
-};
-
-struct PID_POS
-{
-	struct PID pid;
-	double	iSum;
-};
-
-static void PID_init(struct PID *this, double targetPoint)
-{
-	this->targetPoint = targetPoint;
-	this->lastError = 0;
-	this->prevError = 0;
+	if(mode == PID_INC)
+	{
+		this->mode			= mode;
+		this->targetPoint 	= targetPoint;
+		this->currentval 	= 0;
+		this->lastval		= 0;
+		this->last_lastval	= 0;
+	}
+	else if(mode == PID_POS)
+	{
+		this->mode			= mode;
+		this->targetPoint 	= targetPoint;
+		this->currentval 	= 0;
+		this->lastval		= 0;
+		this->last_lastval	= 0;
+		this->iSum			= 0;
+	}
 }
 
-static double PID_outputLimit(struct PID *this, double output)
-{
-	if(output < 0)
-	{
-		output = 0;
-	}
-	else if(output > VAL_MAX)
-	{
-		output = VAL_MAX;
-	}
-
-	return output;
-}
-
-static void PID_setParameter(struct PID *this, float kp, float ki, float kd)
+static void PID_setParameter(pid_ctrl *this, float kp, float ki, float kd)
 {
 	this->kp = kp;
 	this->ki = ki;
 	this->kd = kd;
 }
 
-void PID_setTargetValue(struct PID *this, double targetPoint)
+void PID_setTargetValue(struct PID *this, float targetPoint)
 {
 	this->targetPoint = targetPoint;
 }
@@ -88,116 +76,63 @@ double PID_getTargetValue(struct PID *this)
 	return this->targetPoint;
 }
 
-static double PID_inccalculate(struct PID *this, double currenPoint);
-
-struct PID_INC g_PID_Inc =
+static double PID_calculate(struct PID *this, float currentPoint)
 {
-	.pid = {
-		.mode 			= PID_INC,
+	if(this->mode == PID_INC)
+	{
+		this->currentval = this->targetPoint - currentPoint;
+
+		double out = this->kp * (this->currentval - this->lastval)+\
+					 this->ki * this->currentval+\
+					 this->kd * (this->currentval - 2*this->lastval + this->last_lastval);
+
+		this->last_lastval = this->lastval;
+		this->lastval = this->currentval;
+
+		return out;
+	}
+	else if(this->mode == PID_POS)
+	{
+		this->currentval = this->targetPoint - currentPoint;
+		this->iSum += this->currentval;
+
+		double out = this->kp * this->currentval+\
+					 this->ki * this->iSum+\
+					 this->kd * (this->currentval - this->lastval);
+
+		this->lastval = this->currentval;
+
+		return out;
+	}
+	else
+	{
+		return -1;
+	}
+
+	return -1;
+}
+
+static pid_ctrl g_PID =
+{
 		.init 			= PID_init,
-		.outputLimit 	= PID_outputLimit,
-		.setParameter 	= PID_setParameter,
-		.calculate 		= PID_inccalculate,
-	},
-};
-
-static double PID_inccalculate(struct PID *this, double currenPoint)
-{
-	this->currError = this->targetPoint - currenPoint;
-
-	double out = this->kp * (this->currError - this->lastError) + \
-                 this->ki * this->currError + \
-                 this->kd * (this->currError - 2*this->lastError + this->prevError);
-
-	this->lastError = this->currError;
-	this->prevError = this->lastError;
-
-	if(this->outputLimit)
-	{
-		out = this->outputLimit(this, out);
-	}
-
-	return out;
-}
-
-static void PID_PosInit(struct PID *this, double targetPoint);
-static double PID_PosCalculate(struct PID *this, double currenPoint);
-
-struct PID_POS g_PID_Pos =
-{
-	.pid = {
-		.mode			= PID_POS,
-		.init 			= PID_PosInit,
-		.outputLimit	= PID_outputLimit,
 		.setParameter	= PID_setParameter,
-		.calculate		= PID_PosCalculate,
-	},
+		.claculate		= PID_calculate,
 };
-
-static void PID_PosInit(struct PID *this, double targetPoint)
-{
-		PID_init(this, targetPoint);
-		struct PID_POS *pid_Handle = (struct PID_POS*)this;
-		pid_Handle->iSum = 0;
-}
-
-static double PID_PosCalculate(struct PID *this, double currenPoint)
-{
-	struct PID_POS *pid_Handle = (struct PID_POS*)this;
-
-	this->currError = this->targetPoint - currenPoint;
-	//this->lastError = this->currError;
-
-	pid_Handle->iSum += this->currError;
-
-	double out = this->kp * this->currError + \
-	this->ki * pid_Handle->iSum + \
-	this->kd * (this->currError - this->prevError);
-
-	this->prevError = this->currError;
-
-	if(this->outputLimit)
-	{
-		out = this->outputLimit(this, out);
-	}
-
-	return out;
-}
-
-#define PID_MODE (1)
-
-struct KernelCtrl
-{
-    struct PID *dtvPid;
-    int flag;
-};
-
-static struct KernelCtrl g_m_kernelCtrl;
-
-void Kernel_Ctrl_Init(void)
-{
-#if PID_MODE
-    g_m_kernelCtrl.dtvPid = &g_PID_Inc.pid;
-#else
-    g_m_kernelCtrl.dtvPid = &g_PID_Pos.pid;
-#endif // PID_MODE
-
-}
 
 int main()
 {
     //struct PID this;
-    static struct PID this;
+    pid_ctrl this;
     float curval = 2.1;
     float out = 0.0;
-    Kernel_Ctrl_Init();
 
-    g_m_kernelCtrl.dtvPid->init(&this, 50.0);
-    g_m_kernelCtrl.dtvPid->setParameter(&this, 0.3, 0.5, 0);
+    g_PID.init(&this,50,PID_INC);
+    //g_PID.init(&this,50,PID_POS);
+    g_PID.setParameter(&this, 0.2,0.5,0);
     while(1)
     {
         curval += 1.3;
-        out = g_m_kernelCtrl.dtvPid->calculate(&this, curval);
+        out = g_PID.claculate(&this,curval);
         if(curval > 125.0)
             break;
         printf("out = %f\n", out);
@@ -205,3 +140,7 @@ int main()
     printf("Hello world!\n");
     return 0;
 }
+
+
+
+
